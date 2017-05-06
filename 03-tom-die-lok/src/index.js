e8f110882e677e5d2266d0b7fc12174cfffcf29f
Markus Ebner <mebner.mmt-b2015@fh-salzburg.ac.at>
2017-05-06 13:34:47 +0200

    should work right

--- 03-tom-die-lok/src/index.js
+++ 03-tom-die-lok/src/index.js
@@ -27,12 +27,12 @@ class Bank extends React.Component {
 
     render() {
         let hint;
-        let src = "money-bag.png";
+        //slet src = "money-bag.png";
         let moveAllowed = false;
         if (this.state.tooMuchMoneyToHold)
             hint = "You have no hands free for another bag of money! You have to bring money to the safe!";
 
-        if (this.state.hands.length === 0)
+        if (this.state.hands.length < 2)
             moveAllowed = true;
 
         return (
@@ -46,18 +46,17 @@ class Bank extends React.Component {
                 <p className="hint">{hint}</p>
                 <Heading value="Make money money, make money money money!" />
                 <div className="make-money">
-                    <Money value={"Main Money"} src={"open-money-bag.png"}/>
                     {
-                        this.state.hands.map((idx) => {
-                            return <Money value={"hand"+idx} src={src}/>
+                        this.state.hands.map((value,idx) => {
+                            return <Money value={"hand"+idx} src={value}/>
                         })
                     }
                 </div>
                 <Heading value="Safe:" />
                 <div className="safe">
                     {
-                        this.state.safe.map((idx) => {
-                            return <Money value={"safe"+idx} src={src}/>
+                        this.state.safe.map((value,idx) => {
+                            return <Money value={"safe"+idx} src={value}/>
                         })
                     }
                 </div>
@@ -66,17 +65,21 @@ class Bank extends React.Component {
     };
 
     addMoneyBag = () => {
+        let src = "money-bag.png";
+        if (this.state.hands.length === 0)
+            src = "open-money-bag.png";
         this.setState({
-            hands: [...this.state.hands, this.state.hands.length+1],
+            hands: [...this.state.hands, src],
             safe: this.state.safe,
             tooMuchMoneyToHold: (this.state.hands.length >= 6)
         });
     };
 
     moveMoneyBag = () => {
+        let src = "money-bag.png";
         this.setState({
             hands: [...this.state.hands].splice(0, this.state.hands.length-1),
-            safe: [...this.state.safe, this.state.safe.length + 1],
+            safe: [...this.state.safe, src],
             tooMuchMoneyToHold: (this.state.hands.length -2 >= 6)
         });
     };
